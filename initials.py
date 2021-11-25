3ed682dacef65a760f5ab688c5217624388d4b1d
Lilly <lilly.elouise@gmail.com>
2021-11-25 22:28:47 +0000

    Create initials.py
    Printing my initials in block art with ASCII art

---
+++ initials.py
@@ -0,0 +1,10 @@
+#Lilly D  
+#initials.py
+
+print("L          D D D D")
+print("L          D      D")
+print("L          D      D")
+print("L          D      D")
+print("L          D      D")
+print("L          D      D")
+print("L L L L L  D D D D")
